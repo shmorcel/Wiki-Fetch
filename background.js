f6d9b644c5d4ceeec32ba049aff1905645daa8c1
Sinan <shmorcel@wpi.edu>
2019-06-27 20:07:46 -0400

    added automatic traversal feature as a breadth-first-search

--- background.js
+++ background.js
@@ -1,7 +1,9 @@
 'use strict';
 
 chrome.runtime.onInstalled.addListener(function() {
-  chrome.storage.sync.set({'remove_number': false}, undefined);
+  // chrome.storage.local.set({remove_number: false}, undefined);
+  chrome.storage.local.set({'count': 0});
+  chrome.storage.local.set({'links': []});
   chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
     chrome.declarativeContent.onPageChanged.addRules([{
       conditions: [new chrome.declarativeContent.PageStateMatcher({
@@ -16,7 +18,8 @@ chrome.runtime.onInstalled.addListener(function() {
 chrome.runtime.onMessage.addListener(
   function(message, callback) {
     if (message.type == "runContentScript"){
-      console.log("Content script initiated.");
+      chrome.storage.local.set({'count': 0});
+      chrome.storage.local.set({'links': []});
       chrome.tabs.executeScript({
         file: 'contentScript.js'
       });
@@ -27,6 +30,24 @@ chrome.runtime.onMessage.addListener(
       chrome.downloads.download({
         url: url, // The object URL can be used as download URL
         filename: message.title + ".txt" 
+      });
+    } else if (message.type == "update_tab") {
+      var number_of_pages = 10;
+      chrome.storage.local.get(['count'], function(result) {
+        var count = result.count;
+        if (count < number_of_pages) {
+          chrome.storage.local.get(['links'], function(result) {
+            setTimeout(function(){
+              chrome.tabs.update({url: result.links[count]})
+            }, 1000);
+            setTimeout(function(){
+              chrome.tabs.executeScript({
+                file: 'contentScript.js'
+              });
+            }, 2000);
+          });
+        }
+        chrome.storage.local.set({'count': count + 1});
       });
     }
   }
--- contentScript.js
+++ contentScript.js
@@ -1,11 +1,8 @@
 prepare_text = function (text) {
-  // check if the length is good enough
-  var passes = (text.length > 1);
-  if (!passes) {
-    return null;
-  }
   // process the text before returning it
     // remove the citations
+    var curly_brakets_finder = /{.*}/g;
+    text = text.replace(curly_brakets_finder, "");
     var citation_finder = /\[\d*\]/g;
     text = text.replace(citation_finder, "");
     // the range removal has to happen before the number removal
@@ -13,8 +10,20 @@ prepare_text = function (text) {
     text = text.replace(range_finder, "");
     var number_finder = /([\d*].[\d*]|[\d*])/g;
     text = text.replace(number_finder, "");
-    var percentage_finder = /%/g;
-    text = text.replace(percentage_finder, "");
+    var anything_else_finder = /[^a-zA-Z(),.;:"'?!\s]+/g;
+    text = text.replace(anything_else_finder, "");
+    //var percentage_finder = /%/g;
+    //text = text.replace(percentage_finder, "");
+    //var degree_finder = /°/g;
+    //text = text.replace(degree_finder, "");
+    //var square_root_finder = /√/g;
+    //text = text.replace(square_root_finder, "");
+
+  // check if the length is good enough
+  var passes = (text.length > 5);
+  if (!passes) {
+    return null;
+  }
   //return the processed text
   return text;
 }
@@ -29,43 +38,76 @@ prepare_title = function (text){
   return text;
 }
 
-var textContentBody = document.getElementsByClassName("mw-parser-output")[0];
-var textTitle = document.title;
-textTitle = prepare_title(textTitle);
-var children = textContentBody.children;
-var whole_text = ""
-var i = 0
-for (i = 0; i < children.length; i++) {
-  // make sure it's not a title for External Links
-  if (i+1 < children.length && 
-    children[i+1].tagName == "UL" && 
-    children[i].children[0] != undefined && 
-    children[i].children.length == 1 && 
-    children[i].children[0].tagName == "B") {
-    continue;
+grab_page = function () {
+  var textContentBody = document.getElementsByClassName("mw-parser-output")[0];
+  var textTitle = document.title;
+  textTitle = prepare_title(textTitle);
+  var children = textContentBody.children;
+  var whole_text = ""
+  var i = 0
+  for (i = 0; i < children.length; i++) {
+    // make sure it's not a title for External Links
+    if (i+1 < children.length && 
+      children[i+1].tagName == "UL" && 
+      children[i].children[0] != undefined && 
+      children[i].children.length == 1 && 
+      children[i].children[0].tagName == "B") {
+      continue;
+    }
+    // make sure it's not a paragraph with a span inside (for math equations and all)
+    if (children[i].tagName == "P" && children[i].querySelectorAll("span").length > 0)
+      continue;
+    
+    var show = 0;
+    // it's a Paragraph
+    if (children[i].tagName == "P" && children[i].className != "mw-empty-elt") {
+      child = children[i];
+      show = 1;
+    }
+    // it's a Paragraph in a block-quote
+    if (children[i].tagName == "BLOCKQUOTE") {
+      child = children[i].children[0];  
+      show = 1;
+    }
+    if (show == 1) {
+      var text = children[i].innerText || children[i].textContent;
+      text_to_append = prepare_text(text);
+      if (text_to_append != null)
+        whole_text += text_to_append;
+        whole_text += "\n";
+    }
   }
-  var show = 0;
-  // it's a Paragraph
-  if (children[i].tagName == "P" && children[i].className != "mw-empty-elt") {
-    child = children[i];
-    show = 1;
-  }
-  // it's a Paragraph in a block-quote
-  if (children[i].tagName == "BLOCKQUOTE") {
-    child = children[i].children[0];  
-    show = 1;
-  }
-  if (show == 1) {
-    var text = children[i].innerText || children[i].textContent;
-    text_to_append = prepare_text(text);
-    if (text_to_append != null)
-      whole_text += text_to_append;
-      whole_text += "\n";
-  }
+  var message = {};
+  message.type = "download";
+  message.body = whole_text;
+  message.title = textTitle;
+  chrome.runtime.sendMessage(message);
 }
-var message = {};
-message.type = "download";
-message.body = whole_text;
-message.title = textTitle;
-chrome.runtime.sendMessage(message);
 
+get_link_from_current_page = function (){
+  var textContentBody = document.getElementsByClassName("mw-parser-output")[0];
+  var paragraphs = textContentBody.querySelectorAll("p");
+  var links = [];
+  for (i = 0; i < paragraphs.length; i++){
+    for (k = 0; k < paragraphs[i].children.length; k++){
+      if (paragraphs[i].children[k].tagName == "A")
+        links.push(paragraphs[i].children[k].href);
+    }
+  }
+  return links;
+}
+
+main_script = function () {
+  var time_interval = 1000;
+  setTimeout(function(){ grab_page() }, time_interval);
+  setTimeout(function(){ 
+    var links = get_link_from_current_page();
+    chrome.storage.local.get(['links'], function(result) {
+      chrome.storage.local.set({'links': result.links.concat(links)});
+    });
+    message = {};
+    message.type = "update_tab";
+    chrome.runtime.sendMessage(message);
+  }, 2 * time_interval);
+}
+main_script();
